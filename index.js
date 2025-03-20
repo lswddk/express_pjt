e64c0ba6b1c1db282185c4d611660552fcb917a8
lswddk <ezvv0724@gmail.com>
2025-03-20 20:45:27 +0900

    Add db endpoint

--- index.js
+++ index.js
@@ -32,3 +32,26 @@ app.post("/articles", (req, res)=>{
           });
           
 })
+
+app.get('/articles',(req, res)=>{
+    db.all("SELECT * FROM articles",[], (err, rows) => {
+        if (err) {
+          return res.status(500).json({ error: err.message });
+        }
+        res.json(rows);
+      });
+})
+
+app.get('/articles/:id', (req, res)=>{
+    let id = req.params.id
+
+     db.get("SELECT * FROM articles WHERE id = ?", [id], (err, row) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (!row) {
+      return res.status(404).json({ message: "Article not found" });
+    }
+    res.json(row);
+  });
+})
